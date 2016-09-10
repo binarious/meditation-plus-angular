2b5729ea46ba990fa8818f2a598c3f4b79167c06
Martin <bieder.martin@googlemail.com>
2016-09-11 01:06:07 +0200

    fix(deploy): fixes error with semver + verbose output

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_JOB_NUMBER" = "$TRAVIS_BUILD_NUMBER.1" ]
 then
@@ -12,7 +13,7 @@ then
   then
     echo "TEST: Deploying master to test server"
     folder="meditation-plus-test"
-    version=$TRAVIS_BUILD_NUMBER
+    version="$TRAVIS_BUILD_NUMBER.0.0"
   else
     exit
   fi
@@ -23,5 +24,5 @@ then
   echo "{ \"version\": \"$TRAVIS_TAG\" }" > dist/assets/version.json
   tar -czf transfer-client.tgz dist
   scp -o "StrictHostKeyChecking no" transfer-client.tgz jenkins@159.203.6.130:/var/www/meditation-plus
-  ssh -o "StrictHostKeyChecking no" jenkins@159.203.6.130 'cd /var/www/$folder; rm -rf client; mkdir client; tar -xzf transfer-client.tgz -C client --strip 1; rm transfer-client.tgz'
+  ssh -o "StrictHostKeyChecking no" jenkins@159.203.6.130 "cd /var/www/$folder; rm -rf client; mkdir client; tar -xzf transfer-client.tgz -C client --strip 1; rm transfer-client.tgz"
 fi
