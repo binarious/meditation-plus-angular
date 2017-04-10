2d71a47068c17ab38838b4069b0bcb98de1aa169
Sebastian Kussl <sebastian.kussl@yahoo.de>
2017-04-10 16:48:31 +0200

    chore: remove console.log & vibrate param

--- src/sw.js
+++ src/sw.js
@@ -8,8 +8,6 @@ self.addEventListener('push', function(event) {
     data = event.data.json();
   }
 
-  console.log('Received a push message');
-
   var title = data.title || 'Meditation+';
   var body = data.message || '';
   var icon = './assets/icon/android-chrome-192x192.png';
@@ -17,8 +15,7 @@ self.addEventListener('push', function(event) {
   event.waitUntil(
     self.registration.showNotification(title, {
       body: body,
-      icon: icon,
-      vibrate: [200, 100, 200]
+      icon: icon
     })
   );
 });
