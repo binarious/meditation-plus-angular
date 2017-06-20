968ed8a62a986ed53145563a6b9134d25de73292
Sebastian <sebastian.kussl@yahoo.de>
2017-06-20 09:31:52 +0200

    fix: move whitespace to if expression

--- src/app/message/mentions.pipe.ts
+++ src/app/message/mentions.pipe.ts
@@ -15,8 +15,8 @@ export class MentionsPipe implements PipeTransform {
   transform(text: string) {
     return this.sanitizer.bypassSecurityTrustHtml(
       text.replace(/@([a-zA-Z][a-zA-Z0-9-_\.]{3,20}|all)/g, m =>
-        `<a style="text-decoration:none;color:#000;font-weight:bold;" ${
-          m === '@all' ? '#' : 'href="/profile/' + m.substring(1) + '"'
+        `<a style="text-decoration:none;color:#000;font-weight:bold;"${
+          m === '@all' ? '#' : ' href="/profile/' + m.substring(1) + '"'
         }>${m}</a>`
       )
     );
