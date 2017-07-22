c95fc76b035e2a4f3aa76960200195e00864a412
Sebastian <sebastian.kussl@yahoo.de>
2017-07-22 12:35:36 +0200

    temp

--- src/app/appointment/appointment.service.ts
+++ src/app/appointment/appointment.service.ts
@@ -76,4 +76,18 @@ export class AppointmentService {
       })
     });
   }
+
+  public appointmentsTicker(
+     subscription,
+     unsubscribe: Boolean = false,
+     statusOnly: Boolean = false
+  ) {
+    return this.authHttp.post(
+      ApiConfig.url + '/api/settings/appointmentsTicker',
+      JSON.stringify({ subscription, unsubscribe }), {
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
 }
