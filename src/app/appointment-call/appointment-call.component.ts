b0ae93ec1328638aefa7356f6ddc2ab62688568e
Sebastian <sebastian.kussl@yahoo.de>
2017-09-20 11:26:25 +0200

    temp commit

---
+++ src/app/appointment-call/appointment-call.component.ts
@@ -0,0 +1,149 @@
+import { Component, ApplicationRef, ViewChild, ElementRef } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { ActivatedRoute } from '@angular/router';
+import { AppointmentService } from '../appointment/appointment.service';
+import { UserService } from '../user/user.service';
+import { SettingsService } from '../shared/settings.service';
+import * as moment from 'moment-timezone';
+import * as $script from 'scriptjs';
+
+// HACK: for Google APIs
+declare var gapi: any;
+
+@Component({
+  selector: 'appointment-call',
+  templateUrl: './appointment-call.component.html',
+  styleUrls: [
+    './appointment-call.component.styl',
+  ]
+})
+export class AppointmentCallComponent {
+
+  appointment: Object;
+  started: boolean;
+  settings: any;
+
+  loading: boolean = true;
+  initiated: boolean;
+  ended: boolean;
+  error: string;
+
+  updateSubscription;
+
+  constructor(
+    private appointmentService: AppointmentService,
+    private appRef: ApplicationRef,
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private settingsService: SettingsService
+  ) {
+    this.reset();
+    this.settingsService.get()
+      .map(res => res.json())
+      .subscribe(res => this.settings = res);
+
+    this.updateSubscription = Observable.interval(2500)
+      .subscribe(() => this.loadAppointment());
+  }
+
+  reset(): void {
+    this.initiated = false;
+    this.ended = false;
+    this.error = '';
+    this.loading = true;
+    this.loadAppointment();
+  }
+
+  loadAppointment(): void {
+    this.appointmentService.getNow()
+      .subscribe(res => {
+        this.loading = false;
+
+        // activate hangout button only the first time
+        if (!this.appointment) {
+          this.activateHangoutsButton();
+        }
+
+        // don't update if response is null
+        if (res.appointment) {
+          this.appointment = res.appointment;
+          this.started = res.started;
+        }
+      }, err => console.error(err));
+  }
+
+  /**
+   * Display Hangouts Button
+   */
+  activateHangoutsButton(): void {
+    // initialize Google Hangouts Button
+    $script('https://apis.google.com/js/platform.js', () => {
+      // kick in Change Detection
+      this.appRef.tick();
+
+      gapi.hangout.render('hangout-button', {
+        render: 'createhangout',
+        invites: [{ 'id': 'yuttadhammo@gmail.com', 'invite_type': 'EMAIL' }],
+        initial_apps: [{
+          app_id: '211383333638',
+          start_data: 'dQw4w9WgXcQ',
+          app_type: 'ROOM_APP'
+        }],
+        widget_size: 175
+      });
+    });
+  }
+
+  /**
+   * Format a number representing an hour to
+   * a string in the format 'HH:mmm'.
+   *
+   * Example: 700 => '07:00', 55 => '00:55'
+   *
+   * @param  {number} hour Number representing an hour
+   * @return {string}      Formatted String
+   */
+  parseHour(hour: number): string {
+    let res = ('0000' + hour.toString()).slice(-4);
+    return res.slice(0, 2) + ':' + res.slice(2);
+  }
+
+  /**
+   * Parses hour and converts it to local timezone
+   *
+   * @param  {number} hour Number representing an hour
+   * @return {string}      Formatted String
+   */
+  localHour(hour: number): string {
+    if (!this.settings) {
+      return;
+    }
+
+    return moment
+      .tz(this.parseHour(hour), 'HH:mm', this.settings.appointmentsTimezone)
+      .tz(moment.tz.guess())
+      .format('HH:mm');
+  }
+
+  initiateAppointment(): void {
+    this.initiated = true;
+  }
+
+  showError(evt): void {
+    if (!evt || typeof evt !== 'string') {
+      return;
+    }
+
+    this.error = evt;
+  }
+
+  showEndingScreen(): void {
+    this.ended = true;
+  }
+
+  ngOnDestroy(): void {
+    if (this.updateSubscription) {
+      this.updateSubscription.unsubscribe();
+    }
+  }
+}
---
+++ src/app/appointment-call/videochat/videochat.component.ts
@@ -0,0 +1,280 @@
+import { Component, ViewChild, ElementRef, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { VideoChatService } from './videochat.service';
+import { MessageListEntryComponent } from '../../message/list-entry/message-list-entry.component';
+import * as moment from 'moment';
+import * as SimplePeer from 'simple-peer';
+
+@Component({
+  selector: 'videochat',
+  templateUrl: './videochat.component.html',
+  styleUrls: [
+    './videochat.component.styl',
+    '../../message/list-entry/message-list-entry.component.styl',
+    './loading-animation.css'
+  ],
+  providers: [VideoChatService]
+})
+export class VideoChatComponent implements OnInit, OnDestroy {
+  @Input() appointment;
+
+  @Output() error: EventEmitter<Object> = new EventEmitter<Object>();
+  @Output() ended: EventEmitter<Object> = new EventEmitter<Object>();
+
+  @ViewChild('localVideo', {read: ElementRef}) localVideo: ElementRef;
+  @ViewChild('remoteVideo', {read: ElementRef}) remoteVideo: ElementRef;
+
+  rtcInitiator: boolean;
+  rtcStream; // => stream from local webcam
+  rtcPeer;
+
+  supportCamera: boolean;
+  supportMic: boolean;
+
+  cameraOn: boolean = true;
+  micOn: boolean = true;
+
+  // same properties as above only for opponent
+  opponentCamera: boolean = true;
+  opponentMic: boolean = true;
+  opponentProfile;
+
+  connected: boolean;
+
+  currentMessage: string;
+  messages: Object[] = [];
+
+  // UI modelss
+  loadingMessage: string = 'Initializing';
+
+  constructor(public videochatService: VideoChatService) {
+    if (!SimplePeer.WEBRTC_SUPPORT) {
+      this.error.emit('Your browser does not support WebRTC.');
+    }
+  }
+
+  /**
+   * Exit call
+   */
+  exit(gracefully = true): void {
+    if (!this.connected) {
+      return;
+    }
+
+    // destroy RTC connection
+    if (this.rtcPeer) {
+      this.rtcPeer.destroy();
+      this.rtcPeer = null;
+    }
+
+    // stop stream from local webcam
+    if (this.rtcStream) {
+      this.rtcStream.getTracks().map(track => track.stop());
+    }
+
+    if (gracefully) {
+      this.ended.emit();
+    }
+
+    this.videochatService.leave(gracefully);
+  }
+
+  /**
+   * Ask for media permission and then try to join appointment
+   */
+  getMediaPermission(video: boolean = true): Promise<any> {
+    return navigator.mediaDevices.getUserMedia({
+      video: video,
+      audio: true
+    });
+  }
+
+  /**
+   * Open a P2P WebRTC connection for transmitting video, audio and data
+   */
+  connect(): void {
+    if (!this.rtcStream) {
+      this.getMediaPermission();
+    }
+
+    this.connected = false;
+    this.loadingMessage = 'Connecting';
+
+    this.rtcPeer = new SimplePeer({
+      initiator: this.rtcInitiator,
+      stream: this.rtcStream,
+      reconnectTimer: 3000
+    });
+
+    // specify events
+
+    this.rtcPeer.on('signal', data => this.videochatService.signal(data));
+
+    this.rtcPeer.on('connect', () => this.loadingMessage = 'Connected. Waiting for stream.');
+
+    this.rtcPeer.on('stream', stream => {
+      console.log('onstream', stream);
+      // create fresh HTML video element
+      this.connected = true;
+
+      // notify at the beginning if camera is disabled
+      this.videochatService.toggleMedia(this.supportCamera, this.supportMic);
+
+      // listen for interrupts
+      // stream.getTracks().map(track => track.onended = () => setTimeout(() => {
+      //   if (track.readyState === 'ended') {
+      //     this.connected = false;
+      //     this.loadingMessage = 'Lost connection. Please hold on...';
+      //     this.videochatService.ready();
+      //   }
+      // }, 2000));
+
+      this.showStream(this.remoteVideo.nativeElement, stream);
+    });
+
+    this.rtcPeer.on('close', () => {
+      this.loadingMessage = 'Connection was interrupted.'
+      this.rtcPeer.destroy();
+      this.rtcPeer = null;
+    });
+
+    this.rtcPeer.on('error', err => {
+      console.log('rtcerr', err);
+      if (this.rtcPeer.destroyed) {
+        return;
+      }
+
+      this.connected = false;
+      this.rtcPeer.destroy();
+      this.error.emit('An error occurred: ' + err);
+    });
+
+    window['AppointmentPeerObject'] = this.rtcPeer;
+  }
+
+  /**
+   * Toggles camera
+   */
+  toggleCamera(): void {
+    if (!this.rtcStream) return;
+
+    this.cameraOn = !this.cameraOn;
+
+    for (let track of this.rtcStream.getVideoTracks()) {
+      track.enabled = this.cameraOn;
+    }
+
+    this.videochatService.toggleMedia(this.micOn, this.cameraOn);
+  }
+
+  /**
+   * Toggles microphone
+   */
+  toggleMicrophone(): void {
+    if (!this.rtcStream) return;
+
+    this.micOn = !this.micOn;
+
+    for (let track of this.rtcStream.getAudioTracks()) {
+      track.enabled = this.micOn;
+    }
+
+    this.videochatService.toggleMedia(this.micOn, this.cameraOn);
+  }
+
+  /**
+   * General function for displaying Media Stream inside
+   * an HTML <video> element (cross browser support).
+   */
+  showStream(elem: HTMLMediaElement, stream): void {
+   if (typeof(elem.srcObject) !== 'undefined') {
+      elem.srcObject = stream;
+    } else {
+      // use deprecated method for fallback
+      elem.src = window.URL.createObjectURL(stream);
+    }
+  }
+
+  sendMessage(evt) {
+    if (evt) {
+      evt.preventDefault()
+    }
+
+    this.videochatService.message(this.currentMessage);
+    this.currentMessage = '';
+  }
+
+  formatTime(dt): String {
+    return moment(dt).format('HH:mm');
+  }
+
+  /**
+   * Shows stream from local webcam in browser
+   * and joins Appointment Socket.IO room.
+   */
+  initialize(stream, video = true): void {
+    this.supportCamera = video;
+    this.supportMic = true;
+    this.rtcStream = stream;
+    this.showStream(this.localVideo.nativeElement, stream);
+    this.videochatService.join();
+  }
+
+  ngOnInit() {
+    // initiate connection process by asking for access to
+    // camera and microphone first.
+    this.getMediaPermission(true)
+      .then(
+        stream => this.initialize(stream),
+        err => this.getMediaPermission(false)
+          .then(
+            stream => this.initialize(stream, false),
+            err => this.error.emit('Could not get media permission.')
+          )
+      );
+
+    this.videochatService.on('joined')
+      .subscribe(() => {
+        console.log('onjoined');
+        this.videochatService.ready();
+      });
+
+    this.videochatService.on('ready')
+      .subscribe(data => {
+        console.log('onready', data);
+        this.rtcInitiator = data.initiator === true;
+
+        console.log(this.rtcPeer);
+
+        if (data.ready === true) {
+          this.connect();
+        } else {
+          this.loadingMessage = 'Waiting for opponent to join';
+          this.connected = false;
+        }
+      });
+
+    this.videochatService.on('ended').subscribe(() => this.exit());
+
+    this.videochatService.on('signal')
+      .subscribe(data => {
+        console.log('signal', data);
+        if (this.rtcPeer && !this.rtcPeer.destroyed) {
+          this.rtcPeer.signal(data);
+        }
+      });
+
+    this.videochatService.on('message').subscribe(message => this.messages.push(message));
+
+    this.videochatService.on('toggleMedia')
+      .subscribe(res => {
+        console.log(res);
+        this.opponentCamera = res.video;
+        this.opponentMic = res.audio;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.exit(false);
+  }
+}
